ddbd6b8125092dade3ca35e3d7aa0bff668664b9
Kohei Akiyama <kohei.akiyama@outlook.jp>
2023-07-09 13:17:07 +0900

    ドメインモデルを移植

---
+++ src/hooks/useTranslate.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+export const useTranslate = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    // const getCurrentUrl = chrome.tabs.query({
+    //   lastFocusedWindow: true,
+    //   active: true
+    // })
+    const f = async () => {}
+  }, [])
+}
