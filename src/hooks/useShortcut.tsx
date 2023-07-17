4a67d7ddf1a2ddbc760f4200363d96ac2ba419dd
watorutart <watoru374215@gmail.com>
2023-07-17 20:12:00 +0900

    ショートカットキー設定用フックを追加

---
+++ src/hooks/useShortcut.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, type RefObject } from 'react';
+
+type KeybindProps = {
+  altKey?: boolean;
+  ctrlKey?: boolean;
+  metaKey?: boolean;
+  shiftKey?: boolean;
+  key: KeyboardEvent['key'];
+  onKeyDown?: (event: KeyboardEvent) => void;
+  targetRef?: RefObject<HTMLElement>;
+};
+
+function useLatest<T>(value: T) {
+  const ref = useRef(value);
+  ref.current = value;
+  return ref;
+}
+
+export function useKeybind({
+  altKey,
+  ctrlKey,
+  metaKey,
+  shiftKey,
+  key,
+  onKeyDown,
+  targetRef,
+}: KeybindProps) {
+  const onKeyDownLatest = useLatest(onKeyDown);
+  useEffect(() => {
+    const eventListener = (event: KeyboardEvent) => {
+      if (altKey && !event.altKey) return;
+      if (ctrlKey && !event.ctrlKey) return;
+      if (metaKey && !event.metaKey) return;
+      if (shiftKey && !event.shiftKey) return;
+      if (event.key !== key) return;
+      event.preventDefault();
+      onKeyDownLatest.current?.(event);
+    };
+    if (targetRef?.current) {
+      const target = targetRef.current;
+      target.addEventListener('keydown', eventListener);
+      return () => target.removeEventListener('keydown', eventListener);
+    } else {
+      window.addEventListener('keydown', eventListener);
+      return () => window.removeEventListener('keydown', eventListener);
+    }
+  }, [altKey, ctrlKey, key, metaKey, onKeyDownLatest, shiftKey, targetRef]);
+}
