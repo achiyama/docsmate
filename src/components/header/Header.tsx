eedc0b6cf75adc7f10eda1bed6ec528d0784200c
Kohei Akiyama <kohei.akiyama@outlook.jp>
2023-10-25 03:25:27 +0900

    修正

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,12 @@
 export const Header = () => {
   return (
-    <div className="dt-bg-[#000080] dt-py-1 dt-px-2">
-      <div className="dt-text-[#FFD301]">DocsMate</div>
-    </div>
+    <header className="dt-bg-[#6060ff] dt-py-1 dt-px-2">
+      <div className="dt-text-base ">
+        <span className="dt-mr-1">🐶</span>
+        <span className="dt-text-[#FFF]/[0.8] dt-tracking-wide dt-font-bold">
+          DocsMate
+        </span>
+      </div>
+    </header>
   );
 };
